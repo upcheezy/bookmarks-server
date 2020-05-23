cfe9b0a3d6c38f8515e328019e01d7a69770c9c7
Matthew Upchurch <upcheezy@gmail.com>
2020-05-23 14:08:03 -0400

    updated db url

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://postgres:SpiDee11@localhost/bookmarks',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:SpiDee11@localhost/bookmarks',
 }
